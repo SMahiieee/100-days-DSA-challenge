94fc5dafb6c39a80ee1cf9fd923c78a96f4a8699
MAHI SHARMA <mahisharmaprr@gmail.com>
2026-02-04 22:03:47 +0530

    Add files via upload

---
+++ day3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int n, k;
+    int arr[100];
+    int comparisons = 0;
+    int foundIndex = -1;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter key to search: ");
+    scanf("%d", &k);
+    for (int i = 0; i < n; i++) {
+        comparisons++;
+        if (arr[i] == k) {
+            foundIndex = i;
+            break;
+        }
+    }
+
+    if (foundIndex != -1) {
+        printf("Found at index %d\n", foundIndex);
+    } else {
+        printf("Not Found\n");
+    }
+
+    printf("Comparisons = %d\n", comparisons);
+
+    return 0;
+}
